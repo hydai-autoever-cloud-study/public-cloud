27e244b31f277d5e1832ac2f095630bb5f447476
tpdms0159 <tpdms0159@naver.com>
2024-11-27 20:33:59 +0900

    add : docker image build

--- public-cloud/src/main/java/naver/tpdms0159/publiccloud/PublicController.java
+++ public-cloud/src/main/java/naver/tpdms0159/publiccloud/PublicController.java
@@ -1,12 +1,15 @@
 package naver.tpdms0159.publiccloud;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
+
+@CrossOrigin({"localhost:3000"})
 public class PublicController {
     @Autowired
     PublicService publicService;
